c555dbf955b6e75be33641124b9dbc664b05d159
Ryan <njuyyzh@gmail.com>
2014-08-21 11:26:10 -0400

    count and say

---
+++ string/countAndSay.java
@@ -0,0 +1,42 @@
+/**
+ * The count-and-say sequence is the sequence of integers beginning as follows:
+ * 1, 11, 21, 1211, 111221, ...
+ * 
+ * 1 is read off as "one 1" or "11"
+ * 11 is read off as "two 1s" or "21"
+ * 21 is read off as "one 2, then one 1" or "1211"
+ *
+ * Given an integer n, generate the nth sequence.
+ **/
+
+public class Solution {
+    public String countAndSay(int n) {
+        if(n <= 0) {
+            return null;
+        }
+        return cAs("1", n - 1);
+    }
+
+    private String cAs(String sequence, int n) {
+        if ( n == 0) {
+            return sequence;
+        }
+        char[] array = sequence.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        char currentChar = array[0];
+        int count = 1;
+        for(int i=1; i < array.length; i++) {
+            if(currentChar == array[1]) {
+                count++;
+            }else {
+                sb.append(count);
+                sb.append(currentChar);
+                currentChar = array[i];
+                count = 1;
+            }
+        }
+        sb.append(count);
+        sb.append(currentChar);
+        return cAs(sb.toString(), n - 1);
+    }
+}
